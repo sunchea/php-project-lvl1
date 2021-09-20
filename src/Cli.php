c04e3a99e0a94abbdff7da4001f1e3a2e02a5f80
a.zhamkov <a.zhamkov@vpene.ru>
2021-09-20 18:15:54 +0300

    FIX2: fourth commit, step four

--- src/Cli.php
+++ src/Cli.php
@@ -1,10 +1,15 @@
 <?php
-
+/**
+ * Namespace for function welcomePrompt()
+ */
 namespace Brain\Games\Cli;
 
 use function cli\line;
 use function cli\prompt;
 
+/**
+ * Function welcomePrompt()
+ */
 function welcomePrompt()
 {
     line('Welcome to the Brain Game!');
